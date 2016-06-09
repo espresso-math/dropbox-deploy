d4d8051bfb0c7373f7c80744f1927f9711f39132
James Powell <jamespowell.auth@gmail.com>
2016-06-09 20:00:31 +0530

    jsut

--- scripts/dropbox.sh
+++ scripts/dropbox.sh
@@ -4,5 +4,4 @@ wget https://www.dropbox.com/sh/s7krs6j8jyarvzg/AADSMhOj4OrFSjhj6iUXzIIka?dl=1 -
 
 # unzip and cd
 npm install extract-zip -g
-extract-zip download.zip /download
-cd download
+extract-zip download.zip
