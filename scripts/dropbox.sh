930f6794baa8b8f17f2bcacc823230714bac34e7
James Powell <jamespowell.auth@gmail.com>
2016-06-09 19:37:23 +0530

    jsut

--- scripts/dropbox.sh
+++ scripts/dropbox.sh
@@ -4,17 +4,4 @@
 wget https://www.dropbox.com/sh/s7krs6j8jyarvzg/AADSMhOj4OrFSjhj6iUXzIIka?dl=1 -O download.zip
 
 # unzip and cd
-unzip download.zip -d download
-
-cd download
-
-ls -l
-
-bundle install
-
-
-# run jekyll
-jekyll build
-
-# deploy
-surge _site nup.pii.at
+unzip download.zip
