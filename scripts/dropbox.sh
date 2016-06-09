5b305e8b0155fcfe0eddd36ca12036037e5c8e55
James Powell <jamespowell.auth@gmail.com>
2016-06-09 20:30:34 +0530

    jsut

--- scripts/dropbox.sh
+++ scripts/dropbox.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+set -e # halt script on error
 
 # Get file from dropbox
 wget https://www.dropbox.com/sh/s7krs6j8jyarvzg/AADSMhOj4OrFSjhj6iUXzIIka?dl=1 -O download.zip
 
 # unzip and cd
-npm install extract-zip -g
-extract-zip download.zip
+unzip download.zip -d download
+chmod -R 775 download
+
+cd download
