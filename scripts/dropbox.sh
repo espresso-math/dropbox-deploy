80dbd91fc9925cb1124d3ca5fd15b26f54679e61
James Powell <jamespowell.auth@gmail.com>
2016-06-09 15:58:54 +0530

    n

--- scripts/dropbox.sh
+++ scripts/dropbox.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Get file from dropbox
-wget https://www.dropbox.com/sh/s7krs6j8jyarvzg/AADSMhOj4OrFSjhj6iUXzIIka\?dl\=1 -O download.zip
+wget https://www.dropbox.com/sh/s7krs6j8jyarvzg/AADSMhOj4OrFSjhj6iUXzIIka?dl=1 -O download.zip
 
 # unzip and cd
 unzip download.zip -d download
+
 cd download
